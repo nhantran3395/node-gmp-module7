4a5e6d2a592858e15c0d403b5873ecf6075110fa
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-12-19 10:37:58 +0700

    refactor: change naming of middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,11 +1,11 @@
 import morganMiddleware from "./morgan.middleware";
 import errorHandlerMiddeware from "./error-handler.middleware";
 import routeNotExistsHandlerMiddleware from "./route-not-exists-handler.middleware";
-import tokenAuthorizerMiddleware from "./token-authorizer.middleware";
+import tokenValidatorMiddleware from "./token-validator.middleware";
 
 export {
   errorHandlerMiddeware,
   routeNotExistsHandlerMiddleware,
   morganMiddleware,
-  tokenAuthorizerMiddleware,
+  tokenValidatorMiddleware,
 };
